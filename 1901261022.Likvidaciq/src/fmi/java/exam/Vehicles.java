7c6c87ffabb67be065b8c1b8452624c4fff5a699
loleca <63590989+loleca@users.noreply.github.com>
2020-09-08 16:13:09 +0300

    Add files via upload

---
+++ 1901261022.Likvidaciq/src/fmi/java/exam/Vehicles.java
@@ -0,0 +1,24 @@
+package fmi.java.exam;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+class Vehicles {
+    /*
+    Тука мислех, че ще пълня това невероятно чудо на програмирането с данните от текстовия файл и от него да гледам
+     коя година и дали са свободни, но в крайна сметка нищооооо не написах като хората.
+     */
+    public void FillList() {
+        Vehicles[][] vehicleList = {};
+
+        /*
+        Трябваше да има два "for" loop-a един за колоните друг за редовете.
+         */
+
+        for (int i=0; i < ReadFile.FileReader(); i++){
+
+
+        }
+    }
+}
