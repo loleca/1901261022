9270b00b730b3ad052516d8c62414b048d5cf89d
loleca <63590989+loleca@users.noreply.github.com>
2020-09-08 16:07:44 +0300

    Add files via upload

---
+++ src/fmi/java/exam/AutoPark.java
@@ -0,0 +1,19 @@
+package fmi.java.exam;
+
+
+import java.util.Scanner;
+
+public class AutoPark {
+
+    /*
+    Тука щях просто да викам методите от класовете, но както може да забележите писах някви недовършени глупости по
+    класовете и няма как за толкова кратко време да почна на ново.
+     */
+
+    public static void main(String[] args){
+        ReadFile.FileReader();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Въведете година на производство: ");
+        String carInput = input.nextLine();
+    }
+}
